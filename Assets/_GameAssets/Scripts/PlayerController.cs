60a738d45c7c9a4fa8eecab9f9f5613016b1eb49
fatma_turkyilmaz <96384069+FATMATURKYILMAZ@users.noreply.github.com>
2026-04-03 20:17:07 +0300

    V0.4 Updated Project

---
+++ Assets/_GameAssets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform _orientationTransform;
+   private Rigidbody _playerRigidbody;
+
+    private void Awake()
+    {
+        _playerRigidbody=GetComponent<Rigidbody>();
+    }
+}
