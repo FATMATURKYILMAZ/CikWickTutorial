076d3904a8afcb5c1c548a82d61e25d550a661da
fatma_turkyilmaz <96384069+FATMATURKYILMAZ@users.noreply.github.com>
2026-04-08 12:10:57 +0300

    v0.4 updated project

--- Assets/_GameAssets/Scripts/PlayerController.cs
+++ Assets/_GameAssets/Scripts/PlayerController.cs
@@ -2,11 +2,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private Transform _orientationTransform;
-   private Rigidbody _playerRigidbody;
+    
+    [Header("Movement Settings")]
+    [SerializeField] private float _movementSpeed;
+    private Rigidbody _playerRigidbody;
+    private float _horizontalInput, _verticalInput;
+    private Vector3 _movementDirection;
 
     private void Awake()
     {
-        _playerRigidbody=GetComponent<Rigidbody>();
+        _playerRigidbody = GetComponent<Rigidbody>();
+        _playerRigidbody.freezeRotation=true;
+    }
+    private void Update()
+    {
+        SetInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        SetPlayerMovement();
+    }
+    private void SetInputs()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
+    }
+    private void SetPlayerMovement()
+    {
+        _movementDirection = _orientationTransform.forward * _verticalInput +
+         _orientationTransform.right * _horizontalInput;
+
+        _playerRigidbody.AddForce(_movementDirection * _movementSpeed, ForceMode.Force);
     }
 }
